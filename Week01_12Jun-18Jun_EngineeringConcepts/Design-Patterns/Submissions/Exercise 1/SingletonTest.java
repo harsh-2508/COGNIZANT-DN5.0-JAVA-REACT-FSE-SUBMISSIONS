dce33f726aa6df2dc02a4434221072c5b7142721
Harshvardhan Singh Thakur <hvst032003@gmail.com>
2026-06-15 23:49:44 +0530

    Design Pattern-> Exercise 1

---
+++ Week01_12Jun-18Jun_EngineeringConcepts/Design-Patterns/Submissions/Exercise 1/SingletonTest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class SingletonTest{
+
+  public static void main(String[] args) {
+    Logger logger1=Logger.getInstance();
+    Logger logger2=Logger.getInstance();
+
+    logger1.log("Application Started");
+    logger2.log("User logged in");
+
+    System.out.println(logger1==logger2);
+
+  }
+}
