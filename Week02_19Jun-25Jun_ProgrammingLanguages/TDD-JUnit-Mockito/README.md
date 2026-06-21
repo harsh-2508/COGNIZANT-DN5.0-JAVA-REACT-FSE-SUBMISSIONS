5415aba4f667cca144e299b614015bb60c33514c
Harshvardhan Singh Thakur <hvst032003@gmail.com>
2026-06-21 15:10:14 +0530

    mockito

--- Week02_19Jun-25Jun_ProgrammingLanguages/TDD-JUnit-Mockito/README.md
+++ Week02_19Jun-25Jun_ProgrammingLanguages/TDD-JUnit-Mockito/README.md
@@ -2,10 +2,20 @@
 **Target dates:** 19–25 Jun 2026
 
 ## 📝 Exercises Solved
+
+
+
+# JUNIT
 - [x] Exercise 1: Setting Up JUnit
 - [x] Exercise 2: Writing Basic JUnit Tests
 - [x] Exercise 3: Assertions in JUnit
 - [x] Exercise 4: Arrange-Act-Assert (AAA) Pattern, Test Fixtures, Setup and Teardown Methods in JUnit
+
+# mockito
+- [] Exercise 1: Mocking and Stubbing
+- [] Exercise 2: Verifying Interactions
+# SL4J
+- [] Exercise 1: Logging Error Messages and Warning Levels
 
 
 
---
+++ Week02_19Jun-25Jun_ProgrammingLanguages/TDD-JUnit-Mockito/mockito/MockitoExample/src/test/java/com/cognizant/MyServiceTest.java
@@ -0,0 +1,26 @@
+package com.cognizant;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.mockito.Mockito.*;
+
+
+public class MyServiceTest {
+  @Test
+  void testExternalApi(){
+    //arrange
+    ExternalApi mockApi=mock(ExternalApi.class);
+    when(mockApi.getData()).thenReturn("Mock Data");
+    MyService service=new MyService(mockApi);
+
+    //act
+    String result=service.fetchData();
+
+    //assert
+    assertEquals("Mock Data",result);
+    
+  }
+}
