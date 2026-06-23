501ad47a9534046b34c1a119565a01a5173cb9b1
Harshvardhan Singh Thakur <hvst032003@gmail.com>
2026-06-23 11:22:18 +0530

    spring core maven exercise 1 done

---
+++ Week03_26Jun-02Jul_SpringCore-Maven-JPA-Hibernate/Spring-Core-Maven/Exercise_1/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,8 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+
+public class BookService {
+  public void displayService(){
+    System.out.println("BookService Created");
+  }
+}
